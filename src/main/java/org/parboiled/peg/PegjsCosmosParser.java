8b21aec0aa268b0f8136c5e5768e22a39a9a0d66
Vinod Pahuja <vinod.pahuja@gmail.com>
2022-12-19 11:45:48 +0530

    Initial import

---
+++ src/main/java/org/parboiled/peg/PegjsCosmosParser.java
@@ -0,0 +1,101 @@
+package org.parboiled.peg;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.parboiled.Node;
+import org.parboiled.Rule;
+import org.parboiled.annotations.BuildParseTree;
+import org.parboiled.peg.util.ParseUtils;
+import org.parboiled.support.ParseTreeUtils;
+import org.parboiled.support.ParsingResult;
+
+@BuildParseTree
+public class PegjsCosmosParser extends BasePegjsParser {
+
+	private static List<String> SKIP_LABELS = new LinkedList<>();
+	static {
+		SKIP_LABELS.add("select_specification");
+		SKIP_LABELS.add("from_specification");
+
+		SKIP_LABELS.add("object_property_list");
+
+		SKIP_LABELS.add("scalar_expression");
+		SKIP_LABELS.add("scalar_binary_or_expression");
+		SKIP_LABELS.add("scalar_binary_and_expression");
+		SKIP_LABELS.add("scalar_binary_equality_expression");
+		SKIP_LABELS.add("scalar_binary_relational_expression");
+		SKIP_LABELS.add("scalar_in_expression");
+		SKIP_LABELS.add("scalar_between_expression");
+		SKIP_LABELS.add("scalar_binary_bitwise_or_expression");
+		SKIP_LABELS.add("scalar_binary_bitwise_xor_expression");
+		SKIP_LABELS.add("scalar_binary_bitwise_and_expression");
+		SKIP_LABELS.add("scalar_binary_shift_expression");
+		SKIP_LABELS.add("scalar_binary_additive_expression");
+		SKIP_LABELS.add("scalar_binary_multiplicative_expression");
+		SKIP_LABELS.add("scalar_unary_expression");
+	}
+
+	private static List<String> SUPRESS_LABELS = new LinkedList<>();
+	static {
+		SUPRESS_LABELS.add("_");
+	}
+
+	private static List<String> SUPRESS_SUB_LABELS = new LinkedList<>();
+	static {
+		SUPRESS_SUB_LABELS.add("select");
+		SUPRESS_SUB_LABELS.add("from");
+		SUPRESS_SUB_LABELS.add("where");
+
+		SUPRESS_SUB_LABELS.add("collection_primary_expression");
+
+		SUPRESS_SUB_LABELS.add("identifier");
+		SUPRESS_SUB_LABELS.add("identifier_name");
+		SUPRESS_SUB_LABELS.add("constant");
+	}
+
+	private static Rule startRule;
+
+	@Override
+	public Rule start() {
+		if (startRule == null) {
+			String pegjs = ParseUtils.read(PegjsCosmosParser.class.getClassLoader().getResource("cosmos.pegjs"));
+			ParsingResult<?> result = ParseUtils.parse(pegjs, PegPegjsParser.class, false);
+			startRule = start("sql", result);
+		}
+		return startRule;
+	}
+
+	protected Rule parseRule(ParsingResult<?> result, Node<?> parent) {
+		Rule rule = super.parseRule(result, parent);
+
+		if (rule != null) {
+			String value = result.inputBuffer.extract(parent.getStartIndex(), parent.getEndIndex());
+			if (SUPRESS_LABELS.contains(value)) {
+				rule.suppressNode();
+			} else if (SUPRESS_SUB_LABELS.contains(value)) {
+				rule.suppressSubnodes();
+			} else if (SKIP_LABELS.contains(value)) {
+				rule.skipNode();
+			}
+		}
+
+		return rule;
+	}
+
+	public static ParsingResult<?> parse(String script) throws Exception {
+		System.out.println("script : " + script);
+		ParsingResult<?> result = ParseUtils.parse(script, PegjsCosmosParser.class, false);
+//		ParsingResult<?> result = ParseUtils.parse(script, PegjsCosmosParser.class, true);
+		String parseTreePrintOut = ParseTreeUtils.printNodeTree(result);
+		System.out.println("tree : " + parseTreePrintOut);
+		return result;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		parse("SELECT * FROM employee");
+
+	}
+
+}
